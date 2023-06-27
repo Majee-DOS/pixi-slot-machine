e04442c26c52cae400a17d9770397eff4235da6c
Majeed <abdulmajeed.hersi@gmail.com>
2023-06-27 04:15:03 +0100

    animation working

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
-import { Application, Sprite, Container, Texture, Ticker } from 'pixi.js'
-import { TweenScene } from "../scenes/TweenScene";
+import { Application, Sprite, Container, Texture} from 'pixi.js'
+//import { TweenScene } from "../scenes/TweenScene";
 import { LoaderScene } from "../scenes/LoaderScene";
-import { Tween, Group } from 'tweedle.js'
-
+import TWEEN from "@tweenjs/tween.js";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 1200,
-	height: 800
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
 
-const sceneTween: TweenScene = new TweenScene(app.screen.width, app.screen.height);
-app.stage.addChild(sceneTween);
+//const sceneTween: TweenScene = new TweenScene(app.screen.width, app.screen.height);
+//app.stage.addChild(sceneTween);
 
-const sceneLoader : LoaderScene = new LoaderScene(app.screen.width, app.screen.height);
+const sceneLoader: LoaderScene = new LoaderScene(app.screen.width, app.screen.height);
 app.stage.addChild(sceneLoader);
 
 const REEL_WIDTH = 150;
 const SYMBOL_SIZE = 150;
 const NUM_REELS = 5;
 interface Reel {
-	container: Container;
-	strip: number[];
-	symbols: Sprite[];
-	position: number;
-	previousPosition?: number;
-  }
+  container: Container;
+  strip: number[];
+  symbols: Sprite[];
+  position: number;
+  previousPosition?: number;
+}
 
 LoaderScene.emitter.on('loaded', () => {
-	onAssetsLoaded();
+  onAssetsLoaded();
 });
 
 function createSlotTextures() {
@@ -48,15 +47,16 @@ function createSlotTextures() {
 
 function createReelContainer(i: number) {
   const rc = new Container();
-  rc.x = i * REEL_WIDTH;
+  rc.x = (app.screen.width - (NUM_REELS * REEL_WIDTH)) / 2 + i * REEL_WIDTH;
   return rc;
 }
+
 
 function createReel(rc: Container, reelStrip: number[]) {
   return {
     container: rc,
     strip: reelStrip,
-    symbols: [] as Sprite[], 
+    symbols: [] as Sprite[],
     position: 0,
     previousPosition: 0,
   };
@@ -70,10 +70,6 @@ function createAndScaleSymbol(slotTextures: Texture[], textureIndex: number) {
   return symbol;
 }
 
-function update(): void {
-	Group.shared.update();
-}
-
 // onAssetsLoaded handler builds the slot machine
 function onAssetsLoaded() {
   const slotTextures = createSlotTextures();
@@ -85,16 +81,16 @@ function onAssetsLoaded() {
     [3, 5, 4, 6, 2, 5, 2, 6, 1, 0],
     [1, 1, 6, 4, 1, 3, 2, 0, 3, 3],
   ];
-  
+
   const reels: Reel[] = [];
   const reelContainer = new Container();
-  
+
   for (let i = 0; i < NUM_REELS; i++) {
     const rc = createReelContainer(i);
     reelContainer.addChild(rc);
 
     const reel = createReel(rc, reelStrips[i]);
-    
+
     for (let j = 0; j < reel.strip.length; j++) {
       const texturesIndex = reel.strip[j];
       const symbol = createAndScaleSymbol(slotTextures, texturesIndex);
@@ -119,37 +115,46 @@ function onAssetsLoaded() {
       const extra = Math.floor(Math.random() * 3);
       const target = r.position + 10 + i * 5 + extra;
       const time = 2000 + i * 250;
-      new Tween(r)
+      new TWEEN.Tween(r)
         .to({ position: target }, time)
         .onComplete(() => {
           if (i === reels.length - 1) {
             running = false;
-			update();
           }
         })
         .start();
     }
   }
 
-  startPlay();
+    // Listen for animate update
+    app.ticker.add(() => {
+      // Update the slots
+      for (let i = 0; i < reels.length; i++) {
+        const r = reels[i];
+        // Update blur filter y amount based on speed
+        r.previousPosition = r.position;
+  
+        // Update symbol positions on reel
+        for (let j = 0; j < r.symbols.length; j++) {
+          const s = r.symbols[j];
+          s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
+        }
+      }
+      // Update tweens group
+      TWEEN.update();
+    });
 
-  Ticker.shared.add(() => {
-    update();
-    //app.ticker.update(); // Add this line to update the ticker
-  });
+  startPlay();
 
   const button: Sprite = new Sprite(Texture.from("startButton.png"));
   button.scale.set(0.25);
-  button.position.set(500, 0);
+  button.position.set(0, 0);
   app.stage.addChild(button);
 
   button.interactive = true;
   button.on("pointerdown", () => {
-	startPlay();
-	console.log("play");
-});
-
-
+    startPlay();
+    console.log("play");
+  });
 
 }
-
